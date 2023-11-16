621f7a5d1afecfa7eaed2ebbec9c07f1a9f7196a
juan <curlydoge107@gmail.com>
2023-11-16 12:09:45 -0400

    Lo mismo de la seccion dao pero en las funcion de crearliberrias

---
+++ src/LibreriaDAO.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class LibreriaDAO {
+    public static void crearLibreriaDB(Libreria lib) {
+        Conexion db_connect = new Conexion();
+        try (Connection conexion = db_connect.get_conConnection()) {
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO libreria (nombre, direccion) VALUES (?,?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, lib.getNombreLibreria());
+                ps.setString(2, lib.getDireccion());
+                ps.executeUpdate();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
+
