c09187a66b58d21e8e27c1ac4731571285c506ce
juan <curlydoge107@gmail.com>
2023-11-16 12:06:51 -0400

    Esta es la conexion para el sql

---
+++ src/Conexion.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection get_conConnection(){
+        Connection conection = null;
+        try{
+            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/venta","root","");
+            if(conection!=null){
+                System.out.println("Conexion exitosa ...... ");
+            }
+        }catch (SQLException e){
+            System.out.println("Error: "+e);
+        }
+        return conection;
+    }
+}
