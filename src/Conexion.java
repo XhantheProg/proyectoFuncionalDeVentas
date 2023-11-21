e3294e3e93152cc4f981cf54ac00e1d78325f527
XhantheProg <curlydoge107@gmail.com>
2023-11-21 00:59:35 -0400

    en esta clase cambie un poco el formato para conectarme a la base de datos + añadi un nuevo metodo llamado cerrarRecursos (sirve para evitar fugas de recursos)

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,8 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class Conexion {
+/*public class Conexion {
 
     public Connection get_conConnection(){
         Connection conection = null;
@@ -15,5 +13,55 @@ public class Conexion {
             System.out.println("Error: "+e);
         }
         return conection;
+    }
+}*/
+
+import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.SQLException;
+
+public class Conexion {
+    public Connection get_conConnection() {
+        Connection connection = null;
+        try {
+            // Asegúrate de reemplazar los valores con los correspondientes a tu configuración
+            String url = "jdbc:mysql://localhost:3306/venta"; // Apunta a la base de datos 'venta'
+            String user = "root";
+            String password = "";
+
+            connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Conexión exitosa a la base de datos 'venta'");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+        return connection;
+    }
+
+
+
+    public static void cerrarRecursos(ResultSet resultSet, Statement statement, Connection connection) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                // Manejo de excepciones
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                // Manejo de excepciones
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                // Manejo de excepciones
+            }
+        }
     }
 }
