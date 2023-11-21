b19f4ba1950f928a35dbdaa7d3bcce687bd27e59
XhantheProg <curlydoge107@gmail.com>
2023-11-21 01:06:51 -0400

    Aqui esta lstas las opciones para la libreria + estan 2 opciones de el menu de libros(en libreriaService tengo que quitar el menu de libros ya que se repite en main)

--- src/Main.java
+++ src/Main.java
@@ -2,135 +2,88 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
+
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LibreriaService libreriaService = new LibreriaService();
+        LibroService libroService = new LibroService();
 
         int opcion;
-        Scanner sc = new Scanner(System.in);
         do {
             System.out.println("\n--- Menú de Biblioteca ---");
             System.out.println("1. Crear Librería");
             System.out.println("2. Editar Librería");
             System.out.println("3. Mostrar Librerías");
             System.out.println("4. Borrar Librerías");
-            System.out.println("5. Salir");
+            System.out.println("5. Libros");
+            System.out.println("6. Salir");
             System.out.print("Elija una opción: ");
 
-            opcion = sc.nextInt();
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Limpiar el buffer
 
             switch (opcion) {
                 case 1:
-                    LibreriaService.crearLibreria();
+                    libreriaService.crearLibreria();
                     break;
-                        case 2:
-                            LibreriaService.editarLibreria();
-                            break;
-                        /*case 3:
-                            mostrarLibrerias();
-                            break;
-                        case 4:
-                            borrarLibrerias();
-                            break;
-                        case 5:
-                            System.out.println("Saliendo del programa...");
-                            break;*/
+                case 2:
+                    libreriaService.editarLibreria();
+                    break;
+                case 3:
+                    libreriaService.mostrarLibrerias();
+                    break;
+                /*case 4:
+                    libreriaService.borrarLibreria();
+                    break;*/
+                case 5:
+                    menuLibros(libroService, scanner);
+                    break;
+                case 6:
+                    System.out.println("Saliendo del programa...");
+                    break;
                 default:
-                    System.out.println("Opción inválida. Por favor, elija una opción del 1 al 5.");
+                    System.out.println("Opción no válida. Intente de nuevo.");
             }
-        } while (opcion != 5);
+        } while (opcion != 6);
     }
 
+    private static void menuLibros(LibroService libroService, Scanner scanner) {
+        int opcionLibros;
+        do {
+            System.out.println("\n--- Menú de Libros ---");
+            System.out.println("1. Crear Libro");
+            System.out.println("2. Editar Libro");
+            System.out.println("3. Mostrar Libros");
+            System.out.println("4. Borrar Libro");
+            System.out.println("5. Volver");
+            System.out.print("Elija una opción: ");
 
+            opcionLibros = scanner.nextInt();
+            scanner.nextLine(); // Limpiar el buffer
 
-
-            /*private void editarLibreria() {
-                System.out.println("\n--- Editar Librería ---");
-                System.out.print("Ingrese el ID de la librería a editar: ");
-                int id = obtenerEntero();
-
-                for (Libreria libreria : librerias) {
-                    if (libreria.getId() == id) {
-                        System.out.print("Ingrese el nuevo nombre de la librería (presione Enter para no cambiar): ");
-                        String nuevoNombre = scanner.nextLine();
-                        if (!nuevoNombre.isEmpty()) {
-                            libreria.setNombre(nuevoNombre);
-                        }
-
-                        System.out.print("Ingrese la nueva dirección de la librería (presione Enter para no cambiar): ");
-                        String nuevaDireccion = scanner.nextLine();
-                        if (!nuevaDireccion.isEmpty()) {
-                            libreria.setDireccion(nuevaDireccion);
-                        }
-
-                        System.out.println("Librería editada correctamente.");
-                        return;
-                    }
-                }
-
-                System.out.println("No se encontró ninguna librería con el ID proporcionado.");
+            switch (opcionLibros) {
+                case 1:
+                    libroService.crearLibro();
+                    break;
+                case 2:
+                    libroService.editarLibro();
+                    break;
+                /*case 3:
+                    libroService.mostrarLibros();
+                    break;
+                case 4:
+                    libroService.borrarLibro();
+                    break;*/
+                case 5:
+                    System.out.println("Volviendo al menú principal...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
             }
-
-            private void mostrarLibrerias() {
-                System.out.println("\n--- Mostrar Librerías ---");
-                System.out.println("Total de librerías creadas: " + librerias.size());
-            }
-
-            private void borrarLibrerias() {
-                System.out.println("\n--- Borrar Librerías ---");
-                System.out.println("1. Borrar una librería específica");
-                System.out.println("2. Borrar todas las librerías");
-                System.out.print("Elija una opción: ");
-
-                int opcion = obtenerEntero();
-                switch (opcion) {
-                    case 1:
-                        borrarLibreriaEspecifica();
-                        break;
-                    case 2:
-                        librerias.clear();
-                        System.out.println("Todas las librerías han sido borradas.");
-                        break;
-                    default:
-                        System.out.println("Opción inválida.");
-                }
-            }
-
-            private void borrarLibreriaEspecifica() {
-                System.out.println("\n--- Borrar una Librería Específica ---");
-                System.out.print("Ingrese el ID de la librería a borrar: ");
-                int id = obtenerEntero();
-
-                for (int i = 0; i < librerias.size(); i++) {
-                    if (librerias.get(i).getId() == id) {
-                        librerias.remove(i);
-                        System.out.println("Librería eliminada correctamente.");
-                        return;
-                    }
-                }
-
-                System.out.println("No se encontró ninguna librería con el ID proporcionado.");
-            }
-
-            private int obtenerEntero() {
-                int num;
-                while (true) {
-                    try {
-                        num = scanner.nextInt();
-                        scanner.nextLine(); // Limpiar el buffer del scanner
-                        break;
-                    } catch (InputMismatchException e) {
-                        System.out.println("Por favor, ingrese un número válido.");
-                        scanner.nextLine(); // Limpiar el buffer del scanner
-                    }
-                }
-                return num;
-            }
-        }
-
-        /*public class MenuBiblioteca {
-            public static void main(String[] args) {
-                Biblioteca biblioteca = new Biblioteca();
-                biblioteca.mostrarMenu();
-            }
-        }*/
+        } while (opcionLibros != 5);
     }
+}
