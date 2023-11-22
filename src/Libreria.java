2d6a8c9853e5efc1154c75e768633e1169d55048
XhantheProg <curlydoge107@gmail.com>
2023-11-22 01:13:24 -0400

    Aqui se añadieron los objetos respectivos para empleado

--- src/Libreria.java
+++ src/Libreria.java
@@ -82,4 +82,42 @@ public class Libreria {
             this.autor = autor;
         }
     }
+
+    public static class Empleado {
+        private String nombre;
+        private String apellido;
+        private String cargo;
+
+        // Constructor
+        public Empleado(String nombre, String apellido, String cargo) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.cargo = cargo;
+        }
+
+        // Getters y setters
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getApellido() {
+            return apellido;
+        }
+
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+
+        public String getCargo() {
+            return cargo;
+        }
+
+        public void setCargo(String cargo) {
+            this.cargo = cargo;
+        }
+    }
 }
