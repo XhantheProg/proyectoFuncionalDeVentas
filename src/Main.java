b25858a42fac45ecd643789c941c437d07495263
XhantheProg <curlydoge107@gmail.com>
2023-11-22 01:14:54 -0400

    Aqui el menu de empleado se añadio + estoy pensando en hacer el menu de libreria un submenu para el otro menu llamdo libreria

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,12 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-
-
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LibreriaService libreriaService = new LibreriaService();
         LibroService libroService = new LibroService();
+        EmpleadoService empleadoService = new EmpleadoService();
 
         int opcion;
         do {
@@ -20,7 +17,8 @@ public class Main {
             System.out.println("3. Mostrar Librerías");
             System.out.println("4. Borrar Librerías");
             System.out.println("5. Libros");
-            System.out.println("6. Salir");
+            System.out.println("6. Empleados");
+            System.out.println("7. Salir");
             System.out.print("Elija una opción: ");
 
             opcion = scanner.nextInt();
@@ -36,20 +34,24 @@ public class Main {
                 case 3:
                     libreriaService.mostrarLibrerias();
                     break;
-                /*case 4:
-                    libreriaService.borrarLibreria();
-                    break;*/
+                case 4:
+                    libreriaService.borrarLibrerias();
+                    break;
                 case 5:
                     menuLibros(libroService, scanner);
                     break;
                 case 6:
-                    System.out.println("Saliendo del programa....");
+                    menuEmpleados(empleadoService, scanner);
+                    break;
+                case 7:
+                    System.out.println("Saliendo del programa...");
                     break;
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
             }
-        } while (opcion != 6);
+        } while (opcion != 7);
     }
+
 
     private static void menuLibros(LibroService libroService, Scanner scanner) {
         int opcionLibros;
@@ -72,12 +74,12 @@ public class Main {
                 case 2:
                     libroService.editarLibro();
                     break;
-                /*case 3:
+                case 3:
                     libroService.mostrarLibros();
                     break;
                 case 4:
                     libroService.borrarLibro();
-                    break;*/
+                    break;
                 case 5:
                     System.out.println("Volviendo al menú principal...");
                     break;
@@ -86,4 +88,40 @@ public class Main {
             }
         } while (opcionLibros != 5);
     }
+
+    public static void menuEmpleados(EmpleadoService empleadoService, Scanner scanner) {
+        int opcionEmpleado;
+        do {
+            System.out.println("\n--- Menú de Empleados ---");
+            System.out.println("1. Añadir Empleado");
+            System.out.println("2. Mostrar Empleado");
+            System.out.println("3. Editar Empleado");
+            System.out.println("4. Borrar Empleado");
+            System.out.println("5. Volver al Menú Principal");
+            System.out.print("Elija una opción: ");
+            opcionEmpleado = scanner.nextInt();
+            scanner.nextLine(); // Limpiar el buffer
+
+            switch (opcionEmpleado) {
+                case 1:
+                    empleadoService.agregarEmpleado(); // Llama al método para agregar un empleado
+                    break;
+                case 2:
+                    // Lógica para mostrar empleados
+                    break;
+                case 3:
+                    // Lógica para editar empleado
+                    break;
+                case 4:
+                    // Lógica para borrar empleado
+                    break;
+                case 5:
+                    System.out.println("Volviendo al Menú Principal...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        } while (opcionEmpleado != 5);
+    }
+
 }
