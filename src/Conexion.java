3f4c2cc4de911d9daf02d5d45d45a1c8eb263dd4
XhantheProg <curlydoge107@gmail.com>
2023-11-22 01:16:38 -0400

    Aqui se hace la conexion con la base de datos

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,24 +1,7 @@
 import java.sql.*;
-
-/*public class Conexion {
-
-    public Connection get_conConnection(){
-        Connection conection = null;
-        try{
-            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/venta","root","");
-            if(conection!=null){
-                System.out.println("Conexion exitosa ...... ");
-            }
-        }catch (SQLException e){
-            System.out.println("Error: "+e);
-        }
-        return conection;
-    }
-}*/
-
 import java.sql.Connection;
-        import java.sql.DriverManager;
-        import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
     public Connection get_conConnection() {
